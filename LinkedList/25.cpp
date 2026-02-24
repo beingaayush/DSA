82245245d9724b72eb0c25accb10c8b031e5f000
beingaayush <beingaayushraj@gmail.com>
2026-02-24 22:05:01 +0530

    reverse nodes in k grpups, Clarified Problem Statement

---
+++ LinkedList/25.cpp
@@ -0,0 +1,15 @@
+// Problem Statement:-
+// You are given the head of a linked list and an integer k.
+// Reverse the nodes of the list k at a time, and return the modified list.
+// Rules:
+// Divide the linked list into consecutive groups of size k.
+// Reverse the nodes in each group.
+// If the number of nodes in the last group is less than k, leave them unchanged.
+// You are not allowed to change the values inside nodes — only rearrange the node connections.
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        
+};
