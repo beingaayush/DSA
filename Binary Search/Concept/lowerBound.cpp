d037516c0ac9a15e3f96ce6460d71cba74dc3845
beingaayush <beingaayushraj@gmail.com>
2026-03-01 18:16:41 +0530

    line updation

--- Binary Search/Concept/lowerBound.cpp
+++ Binary Search/Concept/lowerBound.cpp
@@ -5,16 +5,23 @@ using namespace std;
 class Solution{
     public:
         int lowerBound(vector<int>& arr, int target) {
-            int low = 0, high = arr.size() - 1;
+            int n = arr.size();
+            int low = 0;
+            int high = n - 1;
+            int ans = n;   // default: not found case
 
             while (low <= high) {
                 int mid = low + (high - low) / 2;
 
-            if (arr[mid] >= target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-            }   
-            return low;   
+                if (arr[mid] >= target) {
+                    ans = mid;        // potential answer
+                    high = mid - 1;   // try to find smaller index on left
+                } 
+                else {
+                    low = mid + 1;    // move right
+                }
+            }
+
+            return ans;
         }
 };
