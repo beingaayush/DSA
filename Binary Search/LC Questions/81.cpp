89588ad04f9d1b4b0510018ed4ee87a736cc59e1
beingaayush <beingaayushraj@gmail.com>
2026-03-03 22:49:21 +0530

    LC 81 - problem statement defined

---
+++ Binary Search/LC Questions/81.cpp
@@ -0,0 +1,8 @@
+// Problem Statement:-
+// You are given:
+// An integer array nums sorted in ascending order
+// The array is rotated at some unknown pivot
+// Duplicates are allowed
+// You are also given a target.
+// 👉 Return true if target exists in the array.
+// 👉 Otherwise, return false.
