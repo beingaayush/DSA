4ae00d30270be10d7ff869c747400d8debb9655f
beingaayush <beingaayushraj@gmail.com>
2026-03-04 21:40:57 +0530

    LC 81 - search in rotated sorted array II

--- Binary Search/LC Questions/81.cpp
+++ Binary Search/LC Questions/81.cpp
@@ -6,3 +6,57 @@
 // You are also given a target.
 // 👉 Return true if target exists in the array.
 // 👉 Otherwise, return false.
+
+//Core idea:-
+// Agar nums[mid] == target → mil gaya.
+
+// Agar nums[low] == nums[mid] == nums[high]
+// → duplicates ki wajah se sorted side pata nahi chalega
+// → low++ and high--
+
+// Agar left part sorted hai (nums[low] ≤ nums[mid])
+// target us range me hai → left search
+// warna → right search
+
+// Else right part sorted hoga
+// target us range me hai → right search
+// warna → left search
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        bool search(vector<int> &nums, int target){
+            int n = nums.size();
+            int low = 0, high = n-1;
+            while(low <= high){
+                int mid = (low+high)/2;
+                if(nums[mid] == target) return true;
+                //till all 3 are equals shrink the search space
+                if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                    low++;
+                    high--;
+                    continue;
+                }
+                //is left half sorted?
+                if(nums[low] <= nums[mid]){
+                    if(nums[low] <= target && target <= nums[mid]){
+                        high = mid - 1;
+                    }
+                    else{
+                        low = mid + 1;
+                    }
+                }
+                //is right half sorted?
+                else{
+                    if(nums[mid] <= target && target <= nums[high]){
+                        low = mid + 1;
+                    }
+                    else{
+                        high = mid - 1;
+                    }
+                }
+            }
+            return false;
+        }
+};
