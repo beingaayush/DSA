fdcb41633a01332d4434364ae68d4d1d8a78ca42
beingaayush <beingaayushraj@gmail.com>
2026-03-19 16:47:06 +0530

    code updated

--- Recursion/Foundation/printNto1.cpp
+++ Recursion/Foundation/printNto1.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-void fn(int i, int n){
-    if(i<1) return;      //base case: stop when i < 1
-    cout << i << endl;
-    fn(i-1,n);
+void fn(int n){
+    if(n<1) return;      //base case: stop when i < 1
+    cout << n << endl;
+    fn(n-1);
 }
 
 int main(){
     int n;
     cout<<"enter n: ";
     cin >> n;
-    fn(n,n);  //start printing from n
+    
+    fn(n);  //start printing from n
     return  0;
 }
