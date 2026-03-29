1beb86c8a3a9f2f999c344326c3e93be4c6b0196
beingaayush <beingaayushraj@gmail.com>
2026-03-29 19:00:07 +0530

    Inorder traversal | LRR

--- Tree/Foundation/Binary Tree/Tree Traversal/Inorder.cpp
+++ Tree/Foundation/Binary Tree/Tree Traversal/Inorder.cpp
@@ -17,3 +17,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+//traversal
+void inorder(Node* root){
+    if(root == NULL) return;
+
+    inorder(root->left);
+    cout << root->data << endl;
+    inorder(root->right);
+}
+
+int main(){
+    //building the tree manually
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inorder(root);
+}
