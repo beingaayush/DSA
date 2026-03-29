ee4a97da66cf20e3cebad3c840891d6e0c4ca8f0
beingaayush <beingaayushraj@gmail.com>
2026-03-29 22:02:14 +0530

    updated next line node print using NULL | level order traversal

--- Tree/Foundation/Binary Tree/Tree Traversal/LevelOrder.cpp
+++ Tree/Foundation/Binary Tree/Tree Traversal/LevelOrder.cpp
@@ -50,11 +50,24 @@ void levelOrder(Node* root){
     queue<Node*> q;
 
     q.push(root);
+    q.push(NULL); //for printing on next line
     while(q.size() > 0){
         Node* curr = q.front();
         q.pop();
 
+
         cout << curr->data << " ";
+        if(curr == NULL){
+            if(!q.empty()){
+                cout << endl;
+                q.push(NULL);
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+        
         if(curr->left != NULL) q.push(curr->left);
         if(curr->right != NULL) q.push(curr->right);
     }
