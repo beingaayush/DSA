73ee3e2b45417cfa6c75699f0b66a332b04ede7e
beingaayush <beingaayushraj@gmail.com>
2026-03-29 17:36:09 +0530

    Build a Binary tree using Preorder sequence

--- Tree/Foundation/Binary Tree/buildTree.cpp
+++ Tree/Foundation/Binary Tree/buildTree.cpp
@@ -1,3 +1,4 @@
+// Build a Binary Tree using preorder | root -> left -> right
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,3 +13,27 @@ public:
         left = right = NULL;
     }
 };
+
+static int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+    if(preorder[idx] == -1) return NULL;
+    
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);        //LEFT subtree
+    root->right = buildTree(preorder);      //RIGHT subtree
+
+    return root;
+}
+
+int main (){
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node* root = buildTree(preorder);
+
+    cout << root->data << endl;                //1
+    cout << root->left->data << endl;         //2
+    cout << root->right->data << endl;       //3
+    cout << root->right->left->data << endl;  //4
+
+    return;
+}
