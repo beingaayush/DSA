61b309a1e4efdb8ce0771815a3a0280ee4babc0b
beingaayush <beingaayushraj@gmail.com>
2026-04-10 00:52:04 +0530

    Flatten Binary tree to Linked List | leetcode 114

---
+++ Tree/LC Questions/114.cpp
@@ -0,0 +1,39 @@
+// Problem statement:
+// Given the root of a binary tree, flatten the tree into a linked list in-place.
+
+// The linked list should use the same TreeNode structure.
+// The right child pointer should point to the next node.
+// The left child pointer should always be NULL.
+// The order of nodes should follow preorder traversal (Root → Left → Right).
+
+// Example:
+//     1
+//    / \
+//   2   5
+//  / \   \
+// 3   4   6
+// Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+class Solution{
+    public:
+    TreeNode* nextRight = NULL;
+    
+    void flatten(TreeNode* root) {
+        if(root == NULL) return;
+
+        flatten(root->right);
+        flatten(root->left);
+
+        root->left = NULL;
+        root->right = nextRight;
+        nextRight = root;
+    }
+};
