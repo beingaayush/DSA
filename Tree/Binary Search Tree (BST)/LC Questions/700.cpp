948d473e84264f65e41c009fbd05f41d2959a07b
beingaayush <beingaayushraj@gmail.com>
2026-04-19 23:40:22 +0530

    Search in Binary Search Tree | leetcode 700

--- Tree/Binary Search Tree (BST)/LC Questions/700.cpp
+++ Tree/Binary Search Tree (BST)/LC Questions/700.cpp
@@ -8,3 +8,20 @@
 // Input: root = [4,2,7,1,3], val = 2 | Output:  [2,1,3]
 // Input: root = [4,2,7,1,3], val = 5 | Output: []
 
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL) return NULL;
+        if(root->val == val) return root;
+        if(val < root->val) return searchBST(root->left, val);
+        else return searchBST(root->right, val);
+    }
+};
