dea7738d4e331821fe3e7f95350ca55a5a225558
beingaayush <beingaayushraj@gmail.com>
2026-04-19 23:38:55 +0530

    700 - prblm

---
+++ Tree/Binary Search Tree (BST)/LC Questions/700.cpp
@@ -0,0 +1,10 @@
+// Problem Statement: 
+// You are given the root of a Binary Search Tree (BST) and an integer val.
+// Find the node in the BST whose value equals val and return the subtree rooted at that node.
+// If such a node exists → return that node (i.e., pointer/reference to it)
+// If it does not exist → return NULL
+// Key constraint: - BST property holds → left < root < right
+// Example:
+// Input: root = [4,2,7,1,3], val = 2 | Output:  [2,1,3]
+// Input: root = [4,2,7,1,3], val = 5 | Output: []
+
