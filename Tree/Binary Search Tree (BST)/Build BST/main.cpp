8ef8541bf3c92ddf947090bfaf14f221bfeb2406
beingaayush <beingaayushraj@gmail.com>
2026-04-19 23:28:39 +0530

    bst build

---
+++ Tree/Binary Search Tree (BST)/Build BST/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == NULL) return new Node(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insert(root, 5);
+    root = insert(root, 3);
+    root = insert(root, 7);
+
+    return 0;
+}
