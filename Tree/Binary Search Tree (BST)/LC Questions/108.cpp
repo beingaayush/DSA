56feba9c9b74e045d1b7431b90ff79ff8c78427b
beingaayush <beingaayushraj@gmail.com>
2026-04-20 23:49:04 +0530

    code updated, core idea attached

--- Tree/Binary Search Tree (BST)/LC Questions/108.cpp
+++ Tree/Binary Search Tree (BST)/LC Questions/108.cpp
@@ -2,12 +2,23 @@
 // Given an integer array nums where the elements are sorted in ascending order, convert it into a height-balanced binary search tree (BST).
 // A height-balanced BST is defined as a binary tree in which the depth of the two subtrees of every node never differs by more than 1.
 
+// Core idea:
+// Pick the middle element as root to keep the tree balanced.
+// Left half → build left subtree
+// Right half → build right subtree
+// Apply same logic recursively
+// 👉 Sorted array + middle split = height-balanced BST
+
 #include <bits/stdc++.h>
 using namespace std;
 struct TreeNode{
     int val;
     TreeNode* left;
     TreeNode* right;
+    TreeNode(int x){
+        val = x;
+        left = right = NULL;
+    }
 };
 
 class Solution{
@@ -21,6 +32,7 @@ public:
 
         root->left = solve(nums, low, mid - 1);
         root->right = solve(nums, mid + 1, high);
+        return root;
     }
 
     TreeNode* sortedArrToBST(vector<int> &nums){
