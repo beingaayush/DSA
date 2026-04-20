ce856631e918867d72f803a861dcc9c0bf06e90a
beingaayush <beingaayushraj@gmail.com>
2026-04-20 23:45:22 +0530

    Convert sorted array to BST | leetcode 108

---
+++ Tree/Binary Search Tree (BST)/LC Questions/108.cpp
@@ -0,0 +1,29 @@
+// Problem statement:
+// Given an integer array nums where the elements are sorted in ascending order, convert it into a height-balanced binary search tree (BST).
+// A height-balanced BST is defined as a binary tree in which the depth of the two subtrees of every node never differs by more than 1.
+
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+class Solution{
+public:
+    TreeNode* solve(vector<int> &nums, int low, int high){
+        // base Case
+        if(low > high) return NULL;
+
+        int mid = (low + high)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = solve(nums, low, mid - 1);
+        root->right = solve(nums, mid + 1, high);
+    }
+
+    TreeNode* sortedArrToBST(vector<int> &nums){
+        return solve(nums, 0, nums.size()-1);
+    }
+};
